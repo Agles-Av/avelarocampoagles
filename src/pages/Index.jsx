5c9fcbcb682190b42d3eb7450024a7d4dee1245c
Agles <20223tn005@utez.edu.mx>
2025-01-22 13:03:08 -0600

    mi portafolio

---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+<div className="bg-blue-500 text-white text-center p-10">
+  Portafolio de Agles
+</div>
+
+  )
+}
+
+export default Index
