ebe8e3fb1da4192511c4e0452ca36e9d14843af4
Agles <20223tn005@utez.edu.mx>
2025-01-24 15:53:04 -0600

    [add] description

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Index = () => {
   return (
-<div className="bg-blue-500 text-white text-center p-10">
-  Portafolio de Agles
-</div>
+    <>
+      <div className=" bg-blue-500 text-white text-center p-10">
+        Portafolio de Agles
+      </div>
+      <div className='text-center p-10'>
+        <p>Hola soy un joven Acatlipense con ganas de programar más</p>
+      </div>
+    </>
+
 
   )
 }
